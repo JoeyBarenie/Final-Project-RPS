065e56ba254355381c4f356bd2b9fa021fd3612f
JoeyBarenie <97720003+JoeyBarenie@users.noreply.github.com>
2022-04-20 15:57:31 -0400

    all levels done

--- Levels/Camera.gd
+++ Levels/Camera.gd
@@ -2,6 +2,6 @@ extends Camera2D
 
 
 func _physics_process(_delta):
-	var player = get_node_or_null("/root/Levels/Player_Container/Player")
+	var player = get_node_or_null("/root/Game/Player_Container/Player")
 	if player != null:
 		position = player.position
